1d967e4d9d936e3b154f588f19791ceed8bef386
CzarnyTheStudent <117749331+CzarnyTheStudent@users.noreply.github.com>
2024-10-17 17:35:50 +0200

    Fixed Camera Limits

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -5,9 +5,9 @@ public class CameraMovement : MonoBehaviour
 {
     #region Variables
 
-    public Transform target;  // Obiekt, który kamera ma śledzić (np. gracz)
-    public float smoothSpeed = 0.125f;  // Prędkość płynnego podążania
-    public Vector3 offset;  // Przesunięcie kamery względem obiektu
+    public Transform target;  
+    public float smoothSpeed = 0.125f; 
+    public Vector3 offset; 
 
     private Camera _mainCamera;
     private Bounds _cameraBounds;
@@ -18,16 +18,16 @@ public class CameraMovement : MonoBehaviour
 
     private void Start()
     {
-        // Obliczamy granice kamery w zależności od rozmiaru okna gry
+        
         var height = _mainCamera.orthographicSize;
         var width = height * _mainCamera.aspect;
 
-        // Ustawiamy granice świata
+       
         var minX = BoundsGlobal.WorldBounds.min.x + width;
-        var maxX = BoundsGlobal.WorldBounds.extents.x - width;
+        var maxX = BoundsGlobal.WorldBounds.max.x - width;
 
         var minY = BoundsGlobal.WorldBounds.min.y + height;
-        var maxY = BoundsGlobal.WorldBounds.extents.y - height;
+        var maxY = BoundsGlobal.WorldBounds.max.y - height;
 
         _cameraBounds = new Bounds();
         _cameraBounds.SetMinMax(
@@ -40,23 +40,22 @@ public class CameraMovement : MonoBehaviour
     {
         if (target != null)
         {
-            FollowTarget();  // Śledzenie obiektu (gracza)
+            FollowTarget();  
         }
     }
 
     private void FollowTarget()
     {
-        // Docelowa pozycja kamery
+       
         Vector3 desiredPosition = target.position + offset;
-        // Płynne przejście do nowej pozycji
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        // Upewniamy się, że pozycja kamery mieści się w granicach
+      
         smoothedPosition = GetCameraBounds(smoothedPosition);
-        // Ustawienie pozycji kamery
+        
         transform.position = smoothedPosition;
     }
 
-    // Metoda sprawdzająca granice kamery
+   
     private Vector3 GetCameraBounds(Vector3 position)
     {
         return new Vector3(
